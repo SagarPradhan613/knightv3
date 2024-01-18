5cd3388e101e9e4afbe09b055e417c95f91a829e
SagarPradhan613 <karanpradhan42@gmail.com>
2024-01-18 22:33:53 +0530

    sec page

--- apps/web/src/views/Page.tsx
+++ apps/web/src/views/Page.tsx
@@ -1,8 +1,8 @@
+import { ChainId } from '@pancakeswap/chains'
 import { useTranslation } from '@pancakeswap/localization'
 import { Swap } from '@pancakeswap/widgets-internal'
-import { ChainId } from '@pancakeswap/chains'
-import { useActiveChainId } from 'hooks/useActiveChainId'
 import { EXCHANGE_HELP_URLS } from 'config/constants'
+import { useActiveChainId } from 'hooks/useActiveChainId'
 
 const Page: React.FC<
   React.PropsWithChildren<{
@@ -27,6 +27,7 @@ const Page: React.FC<
 
   return (
     <Swap.Page
+      style={{ background: 'transparent' }}
       removePadding={removePadding}
       noMinHeight={noMinHeight}
       hideFooterOnDesktop={hideFooterOnDesktop}
